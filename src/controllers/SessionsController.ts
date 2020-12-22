eaa5cc8368adc68b48fec1980a6b55fbb48d7ef1
Ricardo <rpsfilho93@gmail.com>
2020-12-22 17:33:27 -0300

    Fix create session for new users

--- src/controllers/SessionsController.ts
+++ src/controllers/SessionsController.ts
@@ -35,12 +35,68 @@ export default class SessionsController {
 
     const { id, name, image } = user;
 
+    const numberOfRentals = await prisma.rental.count({
+      where: {
+        client_id: id,
+      },
+    });
+
+    const carsOrderByOccurrences = await prisma.$queryRaw`
+      SELECT "car_id",
+      COUNT(*) AS "count"
+      FROM "Rental"
+      WHERE "client_id" = ${id}
+      GROUP BY "car_id"
+      ORDER BY "count" DESC`;
+
+    if (carsOrderByOccurrences.length === 0) {
+      return response.json({
+        user: {
+          id,
+          name,
+          email,
+          image_url: image ? `http://192.168.25.234:3333/files/${image}` : null,
+        },
+        token,
+      });
+    }
+
+    console.log(carsOrderByOccurrences);
+
+    const favoriteCar = await prisma.car.findUnique({
+      where: {
+        id: carsOrderByOccurrences[0].car_id,
+      },
+      include: {
+        CarImage: true,
+        specs: {
+          where: {
+            name: 'Fuel',
+          },
+          select: {
+            icon: true,
+          },
+        },
+      },
+    });
+
     return response.json({
       user: {
         id,
         name,
         email,
         image_url: image ? `http://192.168.25.234:3333/files/${image}` : null,
+        rentals: numberOfRentals,
+        favoriteCar: favoriteCar
+          ? {
+            name: favoriteCar.name,
+            brand: favoriteCar.brand,
+            daily_value: favoriteCar.daily_value,
+            fuel: favoriteCar.specs[0].icon,
+            image_url: `http://192.168.25.234:3333/files/${favoriteCar.CarImage[0].name}`,
+            occurrences: carsOrderByOccurrences[0].count,
+          }
+          : null,
       },
       token,
     });
