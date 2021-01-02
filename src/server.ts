6e35238a0a31caf518ac093c6cc35708d489a85a
Ricardo <rpsfilho93@gmail.com>
2021-01-02 20:51:48 -0300

    Add error middleware to server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,8 @@
 import { errors } from 'celebrate';
-import express from 'express';
+import express, { Request, Response, NextFunction } from 'express';
 import routes from './routes';
 import uploadConfig from './config/upload';
+import AppError from './errors/AppError';
 
 const app = express();
 
@@ -12,5 +13,21 @@ app.use('/files', express.static(uploadConfig.uploadsFolder));
 app.use(routes);
 
 app.use(errors());
+
+app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
+  if (err instanceof AppError) {
+    return response.status(err.statusCode).json({
+      status: 'error',
+      message: err.message,
+    });
+  }
+
+  console.log(err);
+
+  return response.status(500).json({
+    status: 'error',
+    message: 'Internal server error.',
+  });
+});
 
 app.listen(Number(process.env.PORT) || 3333);
