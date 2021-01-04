6f0b1c49ec076bf781cc0365161ef4dc076f8eb8
Ricardo <rpsfilho93@gmail.com>
2021-01-04 16:45:31 -0300

    Using S3 bucket to update users avatar

--- src/controllers/AvatarUserController.ts
+++ src/controllers/AvatarUserController.ts
@@ -22,58 +22,58 @@ export default class AvatarUserController {
       return response.status(400).json({ message: 'User not found.' });
     }
 
-    if (process.env.NODE_ENV === 'production') {
-      const client = new aws.S3({
-        region: 'us-east-2',
-      });
-
-      if (user.image) {
-        await client
-          .deleteObject({
-            Bucket: 'app-proffy',
-            Key: user.image,
-          })
-          .promise();
-      }
-
-      const originalPath = path.resolve(uploadConfig.tmpFolder, filename);
-
-      const ContentType = mime.getType(originalPath);
-
-      if (!ContentType) {
-        throw new Error('File not found.');
-      }
-
-      const fileContent = await fs.promises.readFile(originalPath);
-
-      await client
-        .putObject({
-          Bucket: 'app-proffy',
-          Key: filename,
-          ACL: 'public-read',
-          Body: fileContent,
-          ContentType,
-        })
-        .promise();
-
-      const updatedUser = await prisma.user.update({
-        where: {
-          id: user_id,
-        },
-        data: {
-          image: filename,
-        },
-      });
-
-      return response.json({
-        ...updatedUser,
-        avatar_url: updatedUser.image
-          ? `${process.env.AWS_URL}/${updatedUser.image}`
-          : null,
-      });
-    }
+    // if (process.env.NODE_ENV === 'production') {
+    const client = new aws.S3({
+      region: 'us-east-2',
+    });
 
     if (user.image) {
+      await client
+        .deleteObject({
+          Bucket: 'rentx',
+          Key: user.image,
+        })
+        .promise();
+    }
+
+    const originalPath = path.resolve(uploadConfig.tmpFolder, filename);
+
+    const ContentType = mime.getType(originalPath);
+
+    if (!ContentType) {
+      throw new Error('File not found.');
+    }
+
+    const fileContent = await fs.promises.readFile(originalPath);
+
+    await client
+      .putObject({
+        Bucket: 'rentx',
+        Key: filename,
+        ACL: 'public-read',
+        Body: fileContent,
+        ContentType,
+      })
+      .promise();
+
+    const updatedUser = await prisma.user.update({
+      where: {
+        id: user_id,
+      },
+      data: {
+        image: filename,
+      },
+    });
+
+    return response.json({
+      ...updatedUser,
+      avatar_url: updatedUser.image
+        ? `${process.env.AWS_URL}/${updatedUser.image}`
+        : null,
+    });
+    // }
+
+    /** if (user.image) {
       const filePath = path.resolve(uploadConfig.uploadsFolder, user.image);
 
       try {
@@ -106,6 +106,6 @@ export default class AvatarUserController {
       image_url: updated.image
         ? `${process.env.APP_API_URL}/files/${updated.image}`
         : null,
-    });
+    });* */
   }
 }
